1484fdb6c6e7f482032d42d3dbbbbf05053dc64f
Ramiro Delgado <ramiddelgado@gmail.com>
2024-02-05 22:23:37 -0300

    Beta final - Arreglo de menu mobile, pasaje de ids a clases, optimizacion del codigo, arreglos varios.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
+const bar = document.querySelector('.bar');
+const close = document.querySelector('.close');
+const nav = document.querySelector('.navbar');
 
 
 if (bar) {          //! Abrir navbar
